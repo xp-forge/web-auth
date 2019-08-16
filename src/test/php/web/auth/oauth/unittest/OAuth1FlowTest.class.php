e950b2aa430c53933d56b7f3083b3d0650ccafac
Timm Friebe <thekid@thekid.de>
2019-08-16 11:03:07 +0200

    Add first tests for OAuth 1.0a flow

---
+++ src/test/php/web/auth/oauth/unittest/OAuth1FlowTest.class.php
@@ -0,0 +1,37 @@
+<?php namespace web\auth\oauth\unittest;
+
+use unittest\TestCase;
+use web\Request;
+use web\Response;
+use web\auth\oauth\OAuth1Flow;
+use web\io\TestInput;
+use web\io\TestOutput;
+use web\session\ForTesting;
+
+class OAuth1FlowTest extends TestCase {
+  const SERVICE = 'https://example.com/oauth';
+  const ID      = 'bf396750';
+  const SECRET  = '5ebe2294ecd0e0f08eab7690d2a6ee69';
+
+  #[@test]
+  public function can_create() {
+    new OAuth1Flow(self::SERVICE, [self::ID, self::SECRET]);
+  }
+
+  #[@test]
+  public function fetches_request_token_then_redirects_to_auth() {
+    $fixture= newinstance(OAuth1Flow::class, [self::SERVICE, [self::ID, self::SECRET]], [
+      'request' => function($path, $token= null, $params= []) {
+        return ['oauth_token' => 'REQUEST-TOKEN'];
+      }
+    ]);
+
+    $req= new Request(new TestInput('GET', '/'));
+    $res= new Response(new TestOutput());
+    $session= (new ForTesting())->create();
+
+    $fixture->authenticate($req, $res, $session);
+
+    $this->assertEquals(self::SERVICE.'/authenticate?oauth_token=REQUEST-TOKEN', $res->headers()['Location']);
+  }
+}
