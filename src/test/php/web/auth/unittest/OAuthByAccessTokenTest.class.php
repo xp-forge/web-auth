a2fb0db77dd035757d59c6ac24796fda3b6986ca
Timm Friebe <thekid@thekid.de>
2023-03-05 11:18:22 +0100

    Migrate to new testing library

--- src/test/php/web/auth/unittest/OAuthByAccessTokenTest.class.php
+++ src/test/php/web/auth/unittest/OAuthByAccessTokenTest.class.php
@@ -2,7 +2,7 @@
 
 use io\streams\MemoryInputStream;
 use peer\http\HttpResponse;
-use unittest\{Assert, Test};
+use test\{Assert, Test};
 use web\auth\oauth\ByAccessToken;
 
 class OAuthByAccessTokenTest {
