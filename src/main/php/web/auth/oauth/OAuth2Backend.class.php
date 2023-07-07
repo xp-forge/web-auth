117aa434ac6c0199f611a5052407015bc6ea45b0
Timm Friebe <thekid@thekid.de>
2023-07-07 17:04:16 +0200

    Extract OAuth 2 backend interaction into dedicated class

---
+++ src/main/php/web/auth/oauth/OAuth2Backend.class.php
@@ -0,0 +1,80 @@
+<?php namespace web\auth\oauth;
+
+use peer\http\HttpConnection;
+use lang\IllegalStateException;
+use io\streams\Streams;
+
+class OAuth2Backend {
+  private $conn, $credentials;
+  private $headers= [];
+
+  /**
+   * Creates a new OAuth 2 backend
+   *
+   * @param  string|util.URI $endpoint
+   * @param  web.auth.oauth.Credentials|(string|util.Secret)[] $credentials
+   */
+  public function __construct($endpoint, $credentials) {
+    $this->conn= new HttpConnection($endpoint);
+
+    // BC: Support web.auth.oauth.Token instances
+    if ($credentials instanceof Credentials) {
+      $this->credentials= $credentials;
+    } else if ($credentials instanceof Token) {
+      $this->credentials= new BySecret($credentials->key()->reveal(), $credentials->secret());
+    } else {
+      $this->credentials= new BySecret(...$credentials);
+    }
+  }
+
+  /**
+   * Specifies headers to add to request
+   *
+   * @param  [:string] $headers
+   * @return self
+   */
+  public function with($headers) {
+    $this->headers= $headers;
+    return $this;
+  }
+
+  /** @return string */
+  public function clientId() { return $this->credentials->key; }
+
+  /**
+   * Performs HTTP request
+   *
+   * @param  [:string] $payload POST parameters
+   * @return [:string] Token
+   * @throws lang.IllegalStateException
+   */
+  protected function request($payload) {
+    $r= $this->conn->post($payload, $this->headers + [
+      'Accept'     => 'application/x-www-form-urlencoded, application/json',
+      'User-Agent' => 'XP/OAuth2'
+    ]);
+
+    $body= Streams::readAll($r->in());
+    if (200 !== $r->statusCode()) {
+      throw new IllegalStateException('Cannot get access token (#'.$r->statusCode().'): '.$body);
+    }
+
+    $type= $r->header('Content-Type')[0];
+    if (strstr($type, 'application/json')) {
+      return json_decode($body, true);
+    } else {
+      parse_str($body, $token);
+      return $token;
+    }
+  }
+
+  /**
+   * Acquires a grant
+   *
+   * @param  [:string] $grant
+   * @return [:string]
+   */
+  public function acquire($grant) {
+    return $this->request($this->credentials->params($this->conn->getUrl()->getCanonicalURL()) + $grant);
+  }
+}
